970ddf20ef6e4e006f8c879dc9906df2137ca2ad
Earl Potters <earl.potters@gmail.com>
2023-05-14 06:06:09 +0200

    Big BIg Commit

--- src/pages/admin/proposals.tsx
+++ src/pages/admin/proposals.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Avatar,
   Box,
@@ -8,12 +8,12 @@ import {
   Select,
   SimpleGrid,
   useColorModeValue,
-} from '@chakra-ui/react';
-import AdminLayout from 'layouts/admin';
-import ProposalCard from 'components/card/ProposalCard';
-import ProposalDetails from 'components/ProposalDetails'; // Import ProposalDetails component
-import proposalsData from 'utils/data/proposals.json'; // replace with actual path
-import Link from 'next/link';
+} from "@chakra-ui/react";
+import AdminLayout from "layouts/admin";
+import ProposalCard from "components/card/ProposalCard";
+import ProposalDetails from "components/ProposalDetails"; // Import ProposalDetails component
+import proposalsData from "utils/data/proposals.json"; // replace with actual path
+import Link from "next/link";
 
 export default function UserReports() {
   const [proposals, setProposals] = useState([]);
@@ -27,20 +27,27 @@ export default function UserReports() {
     setSelectedProposal(proposal);
   };
 
-  const brandColor = useColorModeValue('brand.500', 'white');
-  const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
+  const brandColor = useColorModeValue("brand.500", "white");
+  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
 
   return (
     <AdminLayout>
-      <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 2, '2xl': 6 }} gap="20px" mb="20px">
+      <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 2, "2xl": 6 }}
+          gap="20px"
+          mb="20px"
+        >
           {proposals.map((proposal, index) => (
-            <Link href={`/proposals/${proposal.id}`} key={proposal.id} passHref>
-              <ProposalCard id={proposal.id} title={proposal.title} />
-            </Link>
+            <ProposalCard
+              key={proposal.id}
+              id={proposal.id}
+              title={proposal.title}
+            />
           ))}
         </SimpleGrid>
       </Box>
     </AdminLayout>
   );
 }
+``;
